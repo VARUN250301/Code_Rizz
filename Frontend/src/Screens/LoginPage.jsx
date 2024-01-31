9ce61c410e05cd0b768f227294eb0016a4a96db6
rukshar12 <krukhsaar12@gmail.com>
2024-01-31 13:42:15 +0530

    AuthUpdated

--- Frontend/src/Screens/LoginPage.jsx
+++ Frontend/src/Screens/LoginPage.jsx
@@ -1,89 +1,14 @@
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-
-// function LoginPage() {
-//   const [formData, setFormData] = useState({
-//     username: "",
-//     password: "",
-//   });
-
-//   const handleInputChange = (event) => {
-//     const { name, value } = event.target;
-//     setFormData({
-//       ...formData,
-//       [name]: value,
-//     });
-//   };
-
-//   const navigate = useNavigate();
-//   const handleLogin = () => {
-//     // You can add your login logic here
-//     const { username, password } = formData;
-//     console.log(
-//       `Logging in with username: ${username} and password: ${password}`
-//     );
-//   };
-
-//   const handleRegister = () => {
-//     // Handle registration logic
-//     navigate("/register");
-//   };
-
-//   return (
-//     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-//       <div className="bg-white p-8 rounded shadow-md">
-//         <h2 className="text-2xl font-semibold mb-4">Login</h2>
-//         <div className="mb-4">
-//           <label htmlFor="username" className="block font-semibold mb-2">
-//             Username:
-//           </label>
-//           <input
-//             type="text"
-//             id="username"
-//             name="username"
-//             value={formData.username}
-//             onChange={handleInputChange}
-//             className="w-full border rounded p-2"
-//           />
-//         </div>
-//         <div className="mb-4">
-//           <label htmlFor="password" className="block font-semibold mb-2">
-//             Password:
-//           </label>
-//           <input
-//             type="password"
-//             id="password"
-//             name="password"
-//             value={formData.password}
-//             onChange={handleInputChange}
-//             className="w-full border rounded p-2"
-//           />
-//         </div>
-//         <button
-//           onClick={handleLogin}
-//           className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded"
-//         >
-//           Login
-//         </button>
-//         <p className="hover:cursor-pointer mt-2" onClick={handleRegister}>
-//           Don't have an account? Register
-//         </p>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default LoginPage;
-
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
-
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider, db } from "../firebase";
+import { collection, addDoc, doc, setDoc } from "firebase/firestore";
 import InputControl from "../InputControl/InputControl";
-import { auth } from "../firebase";
-
 import styles from "./Login.module.css";
-import './login.css';
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 function Login() {
   const navigate = useNavigate();
@@ -107,6 +32,12 @@ function Login() {
         setSubmitButtonDisabled(false);
         const user = res.user;
         console.log(user);
+        const userData = {
+          uid: user.uid,
+          email: values.email,
+        };
+
+        const channelRef = await addDoc(collection(db, "users"), userData);
 
         navigate("/home");
       })
@@ -115,63 +46,63 @@ function Login() {
         setErrorMsg(err.message);
       });
   };
+
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider).then(async (res) => {
+        // console.log(result.user);
+        // localStorage.setItem("userId", result.user.uid);
+
+        const user = res.user;
+        console.log(user);
+        const userData = {
+          uid: user.uid,
+          email: values.email,
+        };
+
+        const channelRef = await addDoc(collection(db, "users"), userData);
+        navigate("/home");
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div className='flex items-center bg-gray-100 login'>
-      <div className="absolutee"></div>
-      <div className="bg-white p-8 rounded shadow-md loginDiv">
-       <h2 className="font-semibold mb-4 title">Login</h2>
-       <p className="hover:cursor-pointer mt-2" >
-          Don't have an account? Create your account
-        </p>
-        <div className="mb-4">
+    <div className={styles.container}>
+      <div className={styles.innerBox}>
+        <h1 className={styles.heading}>Login</h1>
+
         <InputControl
+          label="Email"
           onChange={(event) =>
             setValues((prev) => ({ ...prev, email: event.target.value }))
           }
           placeholder="Enter email address"
         />
-        </div>
-        <div className="mb-4">
         <InputControl
+          label="Password"
           onChange={(event) =>
             setValues((prev) => ({ ...prev, pass: event.target.value }))
           }
           placeholder="Enter Password"
         />
-         </div>
 
-         <p className="hover:cursor-pointer mt-2 forgot">
-          Forgot Password?
-        </p>
-
+        <div className={styles.footer}>
           <b className={styles.error}>{errorMsg}</b>
-          <button 
-            className="w-full  hover:bg-blue-600 text-white font-semibold py-2 loginButton"
-            disabled={submitButtonDisabled} onClick={handleSubmission}>
+          <button disabled={submitButtonDisabled} onClick={handleSubmission}>
             Login
           </button>
-          
-          <p className="hover:cursor-pointer mt-10">
-            Or login with 
-            <div className="ButtonsSet">
-              <div><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/900px-Facebook_f_logo_%282019%29.svg.png?20231203063624'></img></div>
-              <div><img src='https://cdn-icons-png.flaticon.com/512/3938/3938028.png'></img></div>
-            </div>
+          <p>
+            Already have an account?{" "}
+            <span>
+              <Link to="/register">Sign up</Link>
+            </span>
           </p>
-      </div>
-
-      <div className="welcome">
-       <p className="hover:cursor-pointer mt-2 title2" >
-          Welcome back 
-        </p>
-        <p className="hover:cursor-pointer mt-2" >
-          Simply create your account by clicking the signup button 
-        </p>
-        <button
-          className="w-full  hover:bg-blue-600 text-white font-semibold py-2 welcomeButton"
-        >
-          <Link to="/register" style={{color:"white",textDecoration:'none'}}>Signup</Link>
-        </button>
+          <div className="auth">
+            <button onClick={signInWithGoogle}> Sign In With Google </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- Frontend/src/Screens/RegistrationForm.jsx
+++ Frontend/src/Screens/RegistrationForm.jsx
@@ -9,37 +9,17 @@ import styles from "./registration.css";
 
 function Register() {
   const navigate = useNavigate();
-  const [values, setValues] = useState({
-    name: "",
+  const [values, setValues] = useState({   
     email: "",
-    location: "",
-    phone: "",
-    blood: "",
     pass: "",
   });
   const [errorMsg, setErrorMsg] = useState("");
   const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
 
-  // Function to get user's location using Geolocation API
-  const getUserLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          const userLocation = `(${latitude}, ${longitude})`;
-          setValues((prev) => ({ ...prev, location: userLocation }));
-        },
-        (error) => {
-          console.error("Error getting user location:", error);
-        }
-      );
-    } else {
-      console.error("Geolocation is not supported by this browser.");
-    }
-  };
+ 
 
   const handleSubmission = async () => {
-    if (!values.name || !values.email || !values.pass) {
+    if (!values.email || !values.pass) {
       setErrorMsg("Fill all fields");
       return;
     }
@@ -51,22 +31,12 @@ function Register() {
         setSubmitButtonDisabled(false);
         const user = res.user;
 
-        await updateProfile(user, {
-          displayName: values.name,
-        });
-
         const userData = {
           uid: user.uid,
-          name: values.name,
           email: values.email,
-          location: values.location,
-          phoneNumber: values.phone,
-          bloodGroup: values.blood,
         };
 
-        await setDoc(doc(db, "users", values.name), {
-          userData,
-        });
+        const channelRef = await addDoc(collection(db, "users"), userData);
         // console.log("User added with ID: ", userRef.id);
 
         navigate("/");
@@ -79,71 +49,52 @@ function Register() {
   };
 
   return (
-    <div style={{display:'flex'}} className="signuppp">
-    <div className="signUpImage"></div>
-    <div className='signUp'>
-      <div className={styles.innerBox}>
-        <h1 className={styles.heading}>SIGN<span style={{color:'black'}}> UP</span></h1>
-        <h2 className={styles.heading}>Start your journey <br/> <span>today.</span></h2>
-        <InputControl
-          label="Name"
-          placeholder="Enter your name"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, name: event.target.value }))
-          }
-        />
-        <InputControl
-          label="Location"
-          placeholder="Enter your location"
-          value={values.location}
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, location: event.target.value }))
-          }
-        />
-        <button onClick={getUserLocation} className="locationButton">Get My Location</button>{" "}
-        {/* Add this button to trigger location retrieval */}
-        <InputControl
-          label="Blood Group"
-          placeholder="Enter your blood group"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, blood: event.target.value }))
-          }
-        />
-        <InputControl
-          label="Phone Number"
-          placeholder="Enter your phone number"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, phone: event.target.value }))
-          }
-        />
-        <InputControl
-          label="Email"
-          placeholder="Enter email address"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, email: event.target.value }))
-          }
-        />
-        <InputControl
-          label="Password"
-          placeholder="Enter password"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, pass: event.target.value }))
-          }
-        />
-        <div className={styles.footer}>
-          <b className={styles.error}>{errorMsg}</b>
-          <button onClick={handleSubmission} disabled={submitButtonDisabled} className="signUpbutton">
-            Signup
-          </button>
-          <p style={{marginLeft:'-25vw',marginTop:'10px'}} className="aldready">
-            Already have an account?{" "}
-            <span>
-              <Link to="/">Login</Link>
-            </span>
-          </p>
+    <div style={{ display: "flex" }} className="signuppp">
+      <div className="signUpImage"></div>
+      <div className="signUp">
+        <div className={styles.innerBox}>
+          <h1 className={styles.heading}>
+            SIGN<span style={{ color: "black" }}> UP</span>
+          </h1>
+          <h2 className={styles.heading}>
+            Start your journey <br /> <span>today.</span>
+          </h2>
+
+          <InputControl
+            label="Email"
+            placeholder="Enter email address"
+            onChange={(event) =>
+              setValues((prev) => ({ ...prev, email: event.target.value }))
+            }
+          />
+          <InputControl
+            label="Password"
+            placeholder="Enter password"
+            onChange={(event) =>
+              setValues((prev) => ({ ...prev, pass: event.target.value }))
+            }
+          />
+          <div className={styles.footer}>
+            <b className={styles.error}>{errorMsg}</b>
+            <button
+              onClick={handleSubmission}
+              disabled={submitButtonDisabled}
+              className="signUpbutton"
+            >
+              Signup
+            </button>
+            <p
+              style={{ marginLeft: "-25vw", marginTop: "10px" }}
+              className="aldready"
+            >
+              Already have an account?{" "}
+              <span>
+                <Link to="/">Login</Link>
+              </span>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }

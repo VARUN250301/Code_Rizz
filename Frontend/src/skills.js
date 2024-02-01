005f24bd2f1176ca9ede604edc86c63ec67d596e
rukshar12 <krukhsaar12@gmail.com>
2024-02-01 08:10:43 +0530

    asdf

---
+++ Frontend/src/skills.js
@@ -0,0 +1,20 @@
+export const skills = [
+  "Environmental",
+  "Human rights",
+  "Improved customer loyalty",
+  "Community Development",
+  "Economic",
+  "Philanthropic",
+  "Charitable giving",
+  "Cost efficiency",
+];
+//     {"skill": "Employee retention rates"},
+//     {"skill": "Engage stakeholders"},
+//     {"skill": "Fair business practices"},
+//     {"skill": "Improving labor policies"},
+//     {"skill": "Innovation"},
+//     {"skill": "Legal responsibility"},
+//     {"skill": "Reducing carbon footprints"},
+//     {"skill": "Set measurable goals"},
+//     {"skill": "Employee benefits"}
+//   ]

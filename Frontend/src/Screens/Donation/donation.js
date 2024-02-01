6723596d26fa6bb3d938fbc664d7a8dd11d9d3d3
Vaibhav Gawad <gawadvaibhavv@gmail.com>
2024-02-01 10:49:26 +0530

    donation done static

--- Frontend/src/Screens/Donation/donation.js
+++ Frontend/src/Screens/Donation/donation.js
@@ -7,72 +7,67 @@ import Trends from "../../components/Category/Trends";
 import slides from "../../mock.json";
 import Navbar from "../../components/Navbar/Navbar";
 import { useAuthListener } from "../../Screens/Home/CurrentUser";
-import { doc, collection, getDoc, onSnapshot, query } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
 import { db } from "../../firebase";
-import Modal from "../Community/Modal";
+import { Modal } from "react-bootstrap";
+
 import React, { useState, useEffect } from "react";
 import { addDoc, updateDoc, setDoc } from "firebase/firestore";
 
 const PopupForm = ({ onClose }) => {
   // Define your form state and logic here
   const [communityName, setCommunityName] = useState("");
-  const [communityAdd, setCommunityAdd] = useState("");
-  const [communityDet, setCommunityDet] = useState("");
+  const [communityemail, setCommunityEmail] = useState("");
   const [formData, setFormData] = useState({
     // communityName: "",
     // communityAdd: "",
     // communityDet: "",
     // Your form fields and their initial values
   });
+
   const user = useAuthListener();
 
-  const handleJoinCommunity = async (e) => {
-    e.preventDefault();
-    try {
-      const communityData = {
-        name1: communityName,
-        location1: communityAdd,
-        details1: communityDet,
-      };
 
-      if (
-        communityName.trim() !== "" &&
-        communityAdd.trim() !== "" &&
-        communityDet.trim() !== ""
-      ) {
-        const channelRef = await addDoc(
-          collection(db, "Community"),
-          communityData
-        );
-      }
+  useEffect(() => {
+    // const unsubscribe = db
+    //   .collection("announcements")
+    //   .orderBy("timestamp", "desc")
+    //   .onSnapshot((snapshot) => {
+    //     const announcementsData = snapshot.docs.map((doc) => ({
+    //       id: doc.id,
+    //       ...doc.data(),
+    //     }));
+    //     setAnnouncements(announcementsData);
+    //   });
 
-      await setDoc(
-        doc(db, `users/${user}/UserCreatedCommunity`, communityName),
-        {
-          communityData,
-        }
-      ).then(() => {
-        onClose();
-      });
+    const ref = query(
+      collection(db, "users",user),
+      orderBy("timestamp", "desc")
+    );
 
-      // await setDoc(doc(db, `users/${user}/UserCreatedCommunity`, communityName), {
-      //   communityData,
-      // });
+    const docSnap = getDocs(ref);
 
-      // await setDoc(doc(db, `community`, communityName), {
-      //   communityData,
-      // });
-      // const userRef = await addDoc(
-      //   communityName,
-      //   collection(db, "users/usersData/joinedCommunity"),
-      //   communityData
-      // );
+    const unsubscribe = onSnapshot(ref, (querySnapshot) => {
+      const announcementsData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setCommunityName(announcementsData.display);
+      setCommunityEmail(announcementsData.setCommunityEmail)
+    });
 
-      //  onClose();
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
+    return () => unsubscribe();
+  }, []);
+
+
 };
 
 function Donation() {
@@ -83,6 +78,11 @@ function Donation() {
   const [community, setCommunity] = useState([]);
   const [isPopupOpenArray, setIsPopupOpenArray] = useState([]);
   const [isPopupOpen, setPopupOpen] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [modalShow, setModalShow] = useState(false);
+  var toggleModal = () => {
+    setModalShow(!modalShow);
+  };
 
   const openPopup = (index) => {
     setIsPopupOpenArray((prev) => {
@@ -121,6 +121,9 @@ function Donation() {
   //   // docSnap.data() will be undefined in this case
   //   console.log("No such document!");
   // }
+  const [index, setIndex] = useState(0);
+
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -154,10 +157,21 @@ function Donation() {
             className="banners row"
             style={{ marginLeft: "250px", width: "86vw" }}
           >
-            {initiatives.map((bd) => (
-              <div className="col-md-4 mb-4">
+            {initiatives.map((bd, index) => (
+              <div
+                onClick={() => {
+                  setModal(true);
+                  setIndex(index);
+                }}
+                className="col-md-4 mb-4"
+              >
                 <div>
-                  <div className="banner" onClick={openPopup}>
+                  <div
+                    className="banner"
+                    onClick={() => {
+                      setModalShow(true);
+                    }}
+                  >
                     <img
                       src={
                         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRECLqvE1Y2jzj36brUEv_gEk5SwF1IepoMOxcZ4n_9oB5U6xNuXZGwE3BIfJi-n6wVFL4&usqp=CAU"
@@ -181,7 +195,9 @@ function Donation() {
                       </div>
                       <div className="spacee2">
                         <div>Days left</div>
-                        <div>{"   "} Completed</div>
+                        <div>
+                          {"   "} {bd.status}
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -195,7 +211,63 @@ function Donation() {
           </div>{" "}
           <br />
         </div>
-        <Modal />
+        {modal && (
+          <Modal show={modalShow} onHide={toggleModal}>
+            <Modal.Header closeButton>
+              <Modal.Title>initiatives</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{/* Add your modal content here */}</Modal.Body>
+            <Modal.Footer>
+              <input type="text" placeholder="Enter your Fund Allocations" />
+              <input
+                type="text"
+                placeholder="Enter Number of Employees participating"
+              />
+              <Button
+                variant="primary"
+                onClick={() => {
+                  initiatives[index].status = "Completed";
+                  setModal(false);
+                  console.log(initiatives[index])
+                  const initiativeEmail = initiatives[index].email;
+                  const initiativeType = initiatives[index].type;
+
+                  // const name =
+
+                  fetch("http://localhost:4000/applications/submit", {
+                    
+                    
+                    method: "POST", 
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                      companyName : "vaibhav Education" ,
+                      companyEmail :"gawadvaibhav31@gmail.com",
+                      initiativeEmail: initiativeEmail,
+                      initiativeType : initiativeType
+
+
+                    }),
+                  })
+                    .then((response) => response.json())
+                    .then((data) => {
+                      alert("Success:", data);
+                    })
+                    .catch((error) => {
+                      alert("Error:", error);
+                    });
+                }}
+              >
+                Join The Social Cause
+              </Button>
+
+              <Button variant="secondary" onClick={() => setModal(false)}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        )}
       </div>
     </div>
 
